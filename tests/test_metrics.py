124f5ef69f0dd0f8db4a16285a66212387ab0d90
Thomas M Kehrenberg <tmke8@posteo.net>
2025-07-11 17:32:36 +0200

    Add tests

---
+++ tests/test_metrics.py
@@ -0,0 +1,88 @@
+from sklearn.metrics import accuracy_score
+from fair_forge import metrics
+import numpy as np
+
+
+def test_renyi():
+    y_true = np.array([1, 0, 1, 0, 1, 0], dtype=np.int32)
+    y_pred = np.array([1, 0, 1, 0, 0, 1], dtype=np.int32)
+    groups = np.array([1, 0, 1, 0, 0, 1], dtype=np.int32)
+
+    renyi_y = metrics.RenyiCorrelation(metrics.DependencyTarget.Y)
+    result = renyi_y(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 3)
+    assert renyi_y.__name__ == "renyi_y"
+
+    renyi_s = metrics.RenyiCorrelation(metrics.DependencyTarget.S)
+    result = renyi_s(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1.0)
+    assert renyi_s.__name__ == "renyi_s"
+
+
+def test_prob_pos():
+    y_true = np.array([1, 0, 1, 0, 1], dtype=np.int32)
+    y_pred = np.array([1, 0, 1, 0, 0], dtype=np.int32)
+    result = metrics.prob_pos(y_true=y_true, y_pred=y_pred)
+    np.testing.assert_allclose(result, 0.4)
+    assert metrics.prob_pos.__name__ == "prob_pos"
+
+
+def test_prob_neg():
+    y_true = np.array([1, 0, 1, 0, 1], dtype=np.int32)
+    y_pred = np.array([1, 0, 1, 0, 0], dtype=np.int32)
+    result = metrics.prob_neg(y_true=y_true, y_pred=y_pred)
+    np.testing.assert_allclose(result, 0.6)
+    assert metrics.prob_neg.__name__ == "prob_neg"
+
+
+def test_per_sens_metrics():
+    y_true = np.array([1, 0, 1, 0, 1, 0], dtype=np.int32)
+    y_pred = np.array([1, 0, 1, 0, 0, 1], dtype=np.int32)
+    groups = np.array([1, 1, 1, 0, 0, 0], dtype=np.int32)
+
+    metric_list = metrics.per_sens_metrics(
+        base_metrics=(accuracy_score, metrics.prob_pos),
+        per_sens=metrics.PerSens.ALL,
+        remove_score_suffix=True,
+    )
+    assert len(metric_list) == 8
+
+    metric = metric_list[0]
+    assert metric.__name__ == "accuracy_diff"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 2 / 3)
+
+    metric = metric_list[1]
+    assert metric.__name__ == "accuracy_ratio"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 3)
+
+    metric = metric_list[2]
+    assert metric.__name__ == "accuracy_min"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 3)
+
+    metric = metric_list[3]
+    assert metric.__name__ == "accuracy_max"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1.0)
+
+    metric = metric_list[4]
+    assert metric.__name__ == "prob_pos_diff"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 3)
+
+    metric = metric_list[5]
+    assert metric.__name__ == "prob_pos_ratio"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 2)
+
+    metric = metric_list[6]
+    assert metric.__name__ == "prob_pos_min"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 1 / 3)
+
+    metric = metric_list[7]
+    assert metric.__name__ == "prob_pos_max"
+    result = metric(y_true=y_true, y_pred=y_pred, groups=groups)
+    np.testing.assert_allclose(result, 2 / 3)
